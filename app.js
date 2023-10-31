df8fb8d5edc1cffb67243d4fcaa0d3fe05e12606
Nurzigit <nurzhigit.turman@narxoz.kz>
2023-10-31 20:28:17 +0600

    add new elements

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const data = [{
-    'name': 'schools',
-    'descriptions':'vfdvbjfdvbjkfdvjkdfnvjkfdnvjdfnvjdf'
-}]
+
 app.use('/css', express.static(__dirname + 'public/css'));
 app.use('/js', express.static(__dirname + 'public/js'));
 app.use('/images', express.static(__dirname + 'public/images'));
@@ -12,11 +9,14 @@ app.use('/images', express.static(__dirname + 'public/images'));
 app.set('views', './views')
 app.set('view engine', 'ejs')
 
-data.map((el) => {
-    app.get('', (req, res) => {
-        res.render('src/about', {text: `${el.name}`})
-    })
+app.get('/', (req, res) => {
+    res.render('index', {text: 'Привет это главная страница'})
 })
+
+app.get('/about', (req, res) => {
+    res.render('src/about', {text: 'Привет это об нас страница'})
+})
+
 
 
 app.listen(port, () => console.log(`Listen port ${port}`));
